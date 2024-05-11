8149a241d9591039dec76f7219ca0a5b70cf0180
tepinly <youssf_fadel@outlook.com>
2024-05-12 00:12:12 +0300

    Replacing ESLint with Biome linter

--- apps/service/src/index.ts
+++ apps/service/src/index.ts
@@ -1,11 +1,11 @@
-import { Hono } from 'hono'
 import { connect } from 'database'
+import { Hono } from 'hono'
 
 const app = new Hono()
 await connect()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+	return c.text('Hello Hono!')
 })
 
 export default app
--- packages/database/lib/mongoose.ts
+++ packages/database/lib/mongoose.ts
@@ -2,10 +2,10 @@ import * as mongoose from 'mongoose'
 import 'dotenv/config'
 
 export const connect = async () => {
-  try {
-    await mongoose.connect(process.env.DATABASE_URL ?? "")
-    console.log('connected to MongoDB')
-  } catch (error) {
-    console.log(error)
-  }
+	try {
+		await mongoose.connect(process.env.DATABASE_URL ?? '')
+		console.log('connected to MongoDB')
+	} catch (error) {
+		console.log(error)
+	}
 }
