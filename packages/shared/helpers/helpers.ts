086b2354440a783d462f50a820fb1b20b455594c
tepinly <youssef_fadel@outlook.com>
2024-05-16 03:15:38 +0300

    Added token generator

--- packages/shared/helpers/helpers.ts
+++ packages/shared/helpers/helpers.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid'
+
 export const isNumeric = (str: string) => {
 	if (typeof str !== 'string') {
 		return false
@@ -9,4 +11,8 @@ export const paginate = (args: { pageNumber?: string; pageSize?: string }) => {
 	const limit = args.pageSize ? +args.pageSize : 20
 	const skip = ((args.pageNumber ? +args.pageNumber : 1) - 1) * limit
 	return [limit, skip]
+}
+
+export const generateToken = () => {
+	return uuidv4()
 }
